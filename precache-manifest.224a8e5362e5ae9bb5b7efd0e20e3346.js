ef78bff9a0f8ce722a6244bd1fdf5e0750539db9
anupammo <a09051985@gmail.com>
2020-03-11 23:17:07 +0530

    Updates

---
+++ precache-manifest.224a8e5362e5ae9bb5b7efd0e20e3346.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed3d488e82b82f08703a04ddc0ecc2b1",
+    "url": "/github.io/saloon-reactjs/index.html"
+  },
+  {
+    "revision": "4b345f22fa0e505b9972",
+    "url": "/github.io/saloon-reactjs/static/css/main.9bc02248.chunk.css"
+  },
+  {
+    "revision": "019fb273ffae870b8da4",
+    "url": "/github.io/saloon-reactjs/static/js/2.924c9b41.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/github.io/saloon-reactjs/static/js/2.924c9b41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4b345f22fa0e505b9972",
+    "url": "/github.io/saloon-reactjs/static/js/main.499fe792.chunk.js"
+  },
+  {
+    "revision": "b0b7cb9ac6becdbdbdd6",
+    "url": "/github.io/saloon-reactjs/static/js/runtime-main.356e79bb.js"
+  }
+]);
